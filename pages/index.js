0b719bcdae3a6336c3cec03f44fd44c5deb89066
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2022-07-23 01:49:13 -0400

    Update index.js
    CC

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { downloadIMG} from '../firebase/utils'
 import { useUser } from '../context/Context.js'
 import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Loader from '../components/Loader'
 import { WithoutAuth } from '../HOCs/WithoutAuth'
 import style from '../styles/Home.module.css'
@@ -8,6 +9,11 @@ import style from '../styles/Home.module.css'
 function Home() {
 
   const { userDB, image, setUserImage } = useUser()
+  const router = useRouter()
+
+  function navToWhastsapp () {
+    router.push(userDB.whatsapp)
+  }
   console.log(userDB)
   useEffect(() => {
     image == null ? downloadIMG(setUserImage): ''
@@ -15,7 +21,7 @@ function Home() {
   return (
     <div style={{  backgroundColor: `${userDB.color}`, minHeight: "100vh"}}>
     {image == null ? <Loader /> : <img src={image} className={style.img} alt="img" />}
-    {userDB.whatsapp && <img src="/whatsapp.svg" className={style.whatsapp} alt="Whatsapp" />}
+    {userDB.whatsapp && <img src="/whatsapp.svg" className={style.whatsapp} onClick={navToWhastsapp} alt="Whatsapp" />}
     </div> 
   )
 }
